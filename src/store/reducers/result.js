f5552e3b09a87232cc6e7bd5c85a2b0fab70907a
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-11-18 21:20:50 +0900

    a learn reducer

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,23 +5,29 @@ const initialState = {
   results: []
 };
 
+const storeResult = (state, action) => {
+  return updateObject(state, {
+    // results: [...state.results, state.counter]
+    results: state.results.concat({id: new Date(), value: action.result})
+  });
+};
+
+const deleteResult = (state, action) => {
+  // const index = 2;
+  // const updatedArray = [...state.results];
+  // updatedArray.splice(index, 1);
+  const updatedArray = state.results.filter(elem => (
+    elem.id !== action.resultID
+  ));
+  return updateObject(state, {results: updatedArray});
+};
+
+// a much leaner reducer can make it easy to check switch cases
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case actionTypes.STORE_RESULT:
-      return updateObject(state, {
-        // results: [...state.results, state.counter]
-        results: state.results.concat({id: new Date(), value: action.result})
-      });
-    case actionTypes.DELETE_RESULT:
-      // const index = 2;
-      // const updatedArray = [...state.results];
-      // updatedArray.splice(index, 1);
-      const updatedArray = state.results.filter(elem => (
-        elem.id !== action.resultID
-      ));
-      return updateObject(state, {results: updatedArray});
-    default:
-      return state;
+    case actionTypes.STORE_RESULT: return storeResult(state, action);
+    case actionTypes.DELETE_RESULT: return deleteResult(state, action);
+    default: return state;
   }
 };
 
