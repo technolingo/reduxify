779f99922ea08226c082f076d596e0cef8a1c07e
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-11-18 19:27:01 +0900

    use a utility func to update objects immutably

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utilities';
 
 const initialState = {
   results: []
@@ -7,11 +8,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
-      return {
-        ...state,
+      return updateObject(state, {
         // results: [...state.results, state.counter]
         results: state.results.concat({id: new Date(), value: action.result})
-      };
+      });
     case actionTypes.DELETE_RESULT:
       // const index = 2;
       // const updatedArray = [...state.results];
@@ -19,10 +19,7 @@ const reducer = (state = initialState, action) => {
       const updatedArray = state.results.filter(elem => (
         elem.id !== action.resultID
       ));
-      return {
-        ...state,
-        results: updatedArray
-      };
+      return updateObject(state, {results: updatedArray});
     default:
       return state;
   }
