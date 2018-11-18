c9b651a420c15e5d6dbb7995dee34b3c19f46da9
ZILONG LI <11096690+technolingo@users.noreply.github.com>
2018-11-18 17:15:59 +0900

    add middleware & redux-devtools ext

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
@@ -14,7 +14,28 @@ const rootReducer = combineReducers({
   ctr: counterReducer,
   res: resultReducer
 });
-const store = createStore(rootReducer);
+
+// middleware
+const logger = store => {
+  return next => {
+    return action => {
+      // do something with the action
+      console.log('[Middleware] dispatching ', action);
+      // pass on the action to the reducer
+      const result = next(action);
+      console.log('[Middleware] next state: ', store.getState());
+      return result;
+    };
+  };
+};
+
+// use || to provide a default compose function as a fallback
+// compose is similar to combineReducers, it combines enhancers
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+// use the applyMiddleware enhancer to pass one
+// or multiple instances of middleware to the store
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
